44c3c932bed736403f3e4af4f0267e9e988df8b3
shj411 <ddongsuk@naver.com>
2024-04-24 15:50:12 +0900

    SecurityConfig 파일 작성

---
+++ src/main/kotlin/com/example/shj411/security/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.example.shj411.security;
+
+import org.springframework.context.annotation.Bean;
+
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+    }
+}
