454449b1a2d1142fc981a61d944d19302b68454d
shj411 <ddongsuk@naver.com>
2024-04-24 15:02:18 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/shj411/Shj411Application.kt
@@ -0,0 +1,11 @@
+package com.example.shj411
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Shj411Application
+
+fun main(args: Array<String>) {
+	runApplication<Shj411Application>(*args)
+}
---
+++ src/test/kotlin/com/example/shj411/Shj411ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.shj411
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Shj411ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
